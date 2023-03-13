bc1dee16bf4007bd4f5ca42c96eeff7d81e6dc28
yamako6321 <124650861+yamako6321@users.noreply.github.com>
2023-03-13 18:32:06 +0700

    3

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 n_days = int(input())
-n_hour = n_days*60
-n_second = n_days*60*60
-v_hour = 38241 #miles per hour
-v_second = v_hour/3600 #miles per second
-length = v_second*n_second #in miles
+n_hour = n_days * 60
+n_second = n_days * 60 * 60
+v_hour = 38241                            # Miles per hour.
+v_second = v_hour / 3600                  # Miles per second.
+length = v_second*n_second                # In miles.
 
-rd_wv_spd_second = 299792458 #in meters per second
-rd_wv_spd_hour = rd_wv_spd_second*3600 #in meters per hour
-cmmnctn_dl = n_hour/rd_wv_spd_hour
+rd_wv_spd_second = 299_792_458            # In meters per second.
+rd_wv_spd_hour = rd_wv_spd_second * 3600  # In meters per hour
+cmmnctn_dl = n_hour / rd_wv_spd_hour
 print(f'in miles: {length}, \n'
-      f'in kilometers: {length * 1.609344}, \n'
-      f'in astronomical units: {length*1.609344*149597870.7}, \n'
+      f'in kilometers: {length * 1.609_344}, \n'
+      f'in astronomical units: {length*1.609_344*149_597_870.7}, \n'
       f'delay: {cmmnctn_dl}')

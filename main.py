fc91e3a1393d48aac4aa4206f42ba00e306fb8fe
yamako6321 <124650861+yamako6321@users.noreply.github.com>
2023-03-13 18:36:37 +0700

    3

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ length = v_second*n_second                # In miles.
 rd_wv_spd_second = 299_792_458            # In meters per second.
 rd_wv_spd_hour = rd_wv_spd_second * 3600  # In meters per hour
 cmmnctn_dl = n_hour / rd_wv_spd_hour
-print(f'in miles: {length}, \n'
-      f'in kilometers: {length * 1.609_344}, \n'
-      f'in astronomical units: {length*1.609_344*149_597_870.7}, \n'
+print(f'in miles: {length} \n'
+      f'in kilometers: {length * 1.609_344} \n'
+      f'in astronomical units: {length*1.609_344*149_597_870.7}54 \n'
       f'delay: {cmmnctn_dl}')
